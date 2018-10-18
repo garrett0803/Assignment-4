ec4dbee6cd3b15b5fd2a35458704a28e49c4adb3
garrett0803 <42852431+garrett0803@users.noreply.github.com>
2018-10-17 21:18:53 -0500

    Assignment 4

--- quadratic function.cpp
+++ quadratic function.cpp
@@ -4,15 +4,32 @@ using namespace std;
 
 int main()
 {
-  int a,b,c;
-  float x,quadfunc;
+  float a,b,c;
+  float quad1,quad2,check;
   cout<<"enter values for a, b, and c: " "\n";
   cin>>a>>b>>c;
-  cout<<"enter your x value: " "\n";
-  cin>>x;
-  quadfunc=quad(a, b, c);
-  cout>>a>>b>>c "\n";
-  cout>>quadfunc "\n";
+
+  quad1= (-b + sqrt((b * b) - (4 * a * c))) / (2 *a);
+  quad2= (-b - sqrt((b * b) - (4 * a * c))) / (2 *a);
+
+  if(check>=0)
+  	{
+    	cout<<"a= "<<a<<"\n";
+      cout<<"b= "<<b<<"\n";
+      cout<<"c= "<<c<<"\n";
+    	cout<<"The answer is: "<<quad1<<"\n";
+      cout<<"Or "<<quad2<<"\n";
+
+  	}
+
+  else
+    {
+      cout<<"no soultion" "\n";
+
+    }
+
+
+
 
   return 0;
 }
